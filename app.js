68897a9e54a8d6a184579bbdbfc0685537705c4e
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-06-25 14:26:39 +0200

    done

--- app.js
+++ app.js
@@ -134,6 +134,10 @@ for (let i = 0; i < pairingArray.length; i++) {
     .classList.add(`fruit-${i + 1}`);
 }
 
+// Le jeu se joue jusqu'a ce que les 18paires du tableau est était trouvées donc count = 18.
+
+let count = 0;
+
 // chacun des tableaux correspond maintenant a une classe = fruit-(number) allant de 1 a 36
 // lorsqu'un élément est cliqué il faut lui retirer la class card et ajouter la class fruit-number;
 
@@ -148,41 +152,123 @@ for (let i = 0; i < pairingArray.length; i++) {
 var isFlipped = false;
 
 let firstTry;
+let firstTryPair;
+
+// window.addEventListener('click', (e) => {
+//   console.log(e.target.);
+// })
 
 section.addEventListener('click', (e) => {
-  console.log(e.target);
-  e.target.style.transform = 'rotateY(180deg)';
-  e.target.style.display = 'none';
-  document.querySelector(
-    `[subindex="${Number(e.target.attributes.frontIndex.value)}`
-  ).style.transform = 'rotateY(180deg)';
-  isFlipped = true;
-  // On recupére la valeure de l'index de l'élement sur lequel on a cliqué.
-  // console.log(Number(e.target.attributes.frontIndex));
-  console.log(Number(e.target.attributes.frontIndex.value));
-  firstTry = Number(e.target.attributes.frontIndex.value);
-  console.log(firstTry);
-  // lorsqu'une card est retournée on changera la variable isFlipped par true.
-  // il faut maintenant itérer dans le tableau jusqua retrouver la paire du numero de la carte retournée.
-});
-
-// console.log(firstTry);
-var cerise = 3;
-console.log(cerise);
-console.log(pairingArray.length);
-for (i = 0; i < pairingArray.length; i++) {
-  for (j = 0; j < pairingArray[i].length; j++) {
-    if (pairingArray[i][j] === cerise) {
-      console.log(pairingArray[i]);
-      for (let x = 0; x < pairingArray[i].length; x++) {
-        if(pairingArray[i][x] != cerise){
-          console.log(pairingArray[i][x]);
+  console.log(e.target.parentElement);
+  if (isFlipped == false) {
+    // e.target.classList.add('showFront');
+    // e.target.parentElement.classList.add('show');
+    e.target.style.transform = 'rotateY(180deg)';
+    e.target.style.display = 'none';
+    document.querySelector(
+      `[subindex="${Number(e.target.attributes.frontIndex.value)}"]`
+    ).style.transform = 'rotateY(180deg)';
+    isFlipped = true;
+    // On recupére la valeure de l'index de l'élement sur lequel on a cliqué.
+    // console.log(Number(e.target.attributes.frontIndex));
+    console.log(Number(e.target.attributes.frontIndex.value));
+    firstTry = Number(e.target.attributes.frontIndex.value);
+    console.log(firstTry);
+    // lorsqu'une card est retournée on changera la variable isFlipped par true.
+    // il faut maintenant itérer dans le tableau jusqua retrouver la paire du numero de la carte retournée.
+    for (i = 0; i < pairingArray.length; i++) {
+      for (j = 0; j < pairingArray[i].length; j++) {
+        if (pairingArray[i][j] === firstTry) {
+          console.log(pairingArray[i]);
+          for (let x = 0; x < pairingArray[i].length; x++) {
+            if (pairingArray[i][x] != firstTry) {
+              console.log(pairingArray[i][x]);
+              firstTryPair = pairingArray[i][x];
+            }
+          }
         }
       }
     }
   }
-}
+});
 
-// On posséde maintenant l'autre numéro de la carte qui a été choisie par le joueur 
-// si la carte que le joueur 
+// Exemple cerise
 
+// console.log(firstTry);
+// Ici on prend l'exemple de la carte numero 3 qui remplace la variable firstTry et de sa
+// paire 'cerisePair'.
+
+// var cerise = 3;
+// var cerisePair;
+// console.log(cerise);
+// console.log(pairingArray.length);
+// for (i = 0; i < pairingArray.length; i++) {
+//   for (j = 0; j < pairingArray[i].length; j++) {
+//     if (pairingArray[i][j] === cerise) {
+//       console.log(pairingArray[i]);
+//       for (let x = 0; x < pairingArray[i].length; x++) {
+//         if(pairingArray[i][x] != cerise){
+//           console.log(pairingArray[i][x]);
+//           cerisePair = pairingArray[i][x];
+//         }
+//       }
+//     }
+//   }
+// }
+
+// On posséde maintenant l'autre numéro de la carte qui a été choisie par le joueur
+// si la carte que le joueur tire par la suite correspond.
+// On créer maintenant une nouvel event au click sur une des cards lorsque une carte est déjà retournée.
+
+// let secondTry;
+
+section.addEventListener('click', (e) => {
+  console.log(e.target);
+  if (
+    isFlipped == true &&
+    Number(e.target.attributes.frontIndex.value) != firstTry
+  ) {
+    e.target.style.transform = 'rotateY(180deg)';
+    e.target.style.display = 'none';
+    secondTry = Number(e.target.attributes.frontIndex.value);
+    document.querySelector(
+      `[subindex="${Number(e.target.attributes.frontIndex.value)}"]`
+    ).style.transform = 'rotateY(180deg)';
+    setTimeout(() => {
+      if (firstTryPair === secondTry) {
+        count += 1;
+        console.log(count);
+      } else {
+        console.log('akalabi');
+        console.log(firstTry);
+        console.log(secondTry);
+        e.target.style.transform = 'rotateY(0deg)';
+        e.target.style.display = 'block';
+        e.target.parentElement.style.transform = 'rotateY(0deg)'
+        document.querySelector(`[frontIndex="${firstTry}"]`).style.transform = 'rotateY(0deg)';
+        document.querySelector(`[frontIndex="${firstTry}"]`).style.display = 'block';
+        console.log(e.target);
+        document.querySelector(
+        `[subindex="${firstTry}"]`
+        ).style.transform = 'rotateY(0deg)';
+      }
+    }, 2000);
+    isFlipped = false;
+  }
+});
+
+// Number(e.target.attributes.frontIndex.value) === firstTryPair
+
+// section.addEventListener('click', (e) => {
+//   console.log(e.target);}
+// )
+
+// frontIndex;
+// e.target.style.transform = 'rotateY(180deg)';
+// e.target.style.display = 'none';
+// document.querySelector(
+//   `[frontIndex="${Number(e.target.attributes.frontIndex.value)}]`
+// ).style.transform = 'rotateY(180deg)';
+// document.querySelector(
+//   `[subindex="${Number(e.target.attributes.frontIndex.value)}]`
+// ).style.transform = 'rotateY(180deg)';
